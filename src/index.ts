41a1a36adcb9ec2d6264ed5758042cd2662fb03c
Sebastian Hübner <25882725+shuebner20@users.noreply.github.com>
2021-07-25 18:45:28 +0200

    update inline help links

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ import { DevopsSourcefileMapPlugin } from "./DevopsSourcefileMapPlugin";
 export function load(app: Application) {
   app.options.addDeclaration({
     name: "devops-sourcefile-url-map",
-    help: "[DevOps Source File Plugin] Pass an array of Azure DevOps URL templates. See https://github.com/shuebner20/typedoc-plugin-devops-sourcefile#advanced-mappings"
+    help: "[DevOps Source File Plugin] Pass an array of Azure DevOps URL templates. See https://github.com/DAVISOL-GmbH/typedoc-plugin-devops-sourcefile#advanced-mappings"
   });
   app.options.addDeclaration({
     name: "devops-sourcefile-url-prefix",
-    help: "[DevOps Source File Plugin] Pass the Azure DevOps URL template. See https://github.com/shuebner20/typedoc-plugin-devops-sourcefile#simple-prefix"
+    help: "[DevOps Source File Plugin] Pass the Azure DevOps URL template. See https://github.com/DAVISOL-GmbH/typedoc-plugin-devops-sourcefile#simple-prefix"
   });
 
   app.converter.addComponent("devops-sourcefile", new DevopsSourcefileMapPlugin(app.converter));
